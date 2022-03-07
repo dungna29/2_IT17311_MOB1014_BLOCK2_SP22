d655fc258ab371fd9b244ee5cb8f97b76a5bb3c6
dungna29 <dungna29@localhost>
2022-03-07 10:40:29 +0700

    Dungna 2022

---
+++ 2_IT17311_MOB1014_BLOCK2_SP2/src/BAI1_LAMQUENVOIJAVA/S1_CacPhimTat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAI1_LAMQUENVOIJAVA;
+
+/**
+ *
+ * @author dungna29
+ */
+public class S1_CacPhimTat {
+    public static void main(String[] args) {
+        //Lý thuyết nằm ở đây
+        System.out.println("Chào các bạn đến với chương trình đầu tiền");
+    }
+}
