cfaa15f8474ed3ef9f5f00e33101eb691de27d01
dungna29 <dungna29@localhost>
2022-03-11 09:31:01 +0700

    Dungna 2022

---
+++ 2_IT17311_MOB1014_BLOCK2_SP2/src/BAI1_LAMQUENVOIJAVA/B1_CacPhimTatTrongNetBeans.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAI1_LAMQUENVOIJAVA;
+
+/**
+ *
+ * @author dungna29
+ */
+public class B1_CacPhimTatTrongNetBeans {
+
+    /*
+        1. psvm + tab || main + tab = Gọi main nhanh
+        2. sout + tab = in ra màn hình
+        3. ctrl + space = gợi ý code
+        4. Shift + F6 = Chạy chương trình hoặc chuột phải vào file đang code chọn run file.
+        5. Ctrl + / = Đóng Mở Comment code
+        6. Ctrl + Shift + Mũi tên lên xuống = Nhân bản dòng hoặc vùng code
+        7. Alt + Shift + f = Clean Code
+     */
+    public static void main(String[] args) {
+        System.out.println("Chào các bạn mong các bạn học tốt java 1");
+        System.out.println("Chào các bạn mong các bạn học tốt java 1");
+        System.out.println("Chào các bạn mong các bạn học tốt java 1");
+        System.out.println("Chào các bạn mong các bạn học tốt java 1");
+        System.out.println("Chào các bạn mong các bạn học tốt java 1");
+        System.out.println("Chào các bạn mong các bạn học tốt java 1");
+        System.out.println("Chào các bạn mong các bạn học tốt java 1");
+        System.out.println("Chào các bạn mong các bạn học tốt java 1");
+        System.out.println("Chào các bạn mong các bạn học tốt java 1");
+        System.out.println("Chào các bạn mong các bạn học tốt java 1");
+        System.out.println("Chào các bạn mong các bạn học tốt java 1");
+        //Bài 1: In 3 thông tin cá nhân bản thân (Tên, Tuổi, Tình trạng hôn nhân)
+        System.out.println("Nguyễn Anh Dũng");
+        System.out.println("18");
+        System.out.println("Không xác định");
+    }
+    /*
+        Người học lập trình có 2 dạng:
+        1. Code
+        2. Chép code
+     */
+}
